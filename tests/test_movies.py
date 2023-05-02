9ac8f845cd2dc445aef3acb4a4da38c9c873ba27
jarretjeter <jarretjeter@yahoo.com>
2023-05-01 23:34:41 -0700

    mock tmdb.Movies object instead of test function

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,8 +1,9 @@
+from collections import namedtuple
 from pathlib import Path
 import sys
 import tmdbsimple as tmdb
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 # Having trouble importing movies module. Add the root project directory to Python path to find it.
 root_dir = str(Path(__file__).resolve().parent.parent)
@@ -21,21 +22,45 @@ class TestMovies(unittest.TestCase):
         mock_discover_movie.assert_called_with(region='US', primary_release_year=1914, include_adult=False, with_runtime_gte='40')
 
         self.assertIsInstance(result, list)
+        # returned result should match expected value
         self.assertEqual(result, [1, 2, 3, 4, 5, 6])
         for item in result:
             self.assertIsInstance(item, int, 'each result item should be int value')
 
-    @patch('movies.get_gen_info')
-    def test_get_gen_info(self, mock_get_gen_info):
-        mock_get_gen_info.return_value = ('The Matrix', 'The Matrix', '1999-03-30', 'en', 'lorem ipsum')
-        result = movies.get_gen_info()
+    @patch('movies.tmdb.Movies')
+    def test_get_gen_info(self, mock_tmdb_Movies):
+        # mock the object returned from tmdb.Movies()
+        movie = MagicMock()
+        # set its attribute values
+        movie.title = 'The Matrix'
+        movie.original_title = 'The Matrix'
+        movie.release_date = '1999-03-30'
+        movie.original_language = 'en'
+        movie.overview = 'Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.'
+        # mocked tmdb.Movies() will return a movie object with the above defined values
+        mock_tmdb_Movies.return_value = movie
+        result = movies.get_gen_info(tmdb.Movies())
         for item in result:
             self.assertIsInstance(item, str, 'each result item is a str value')
 
-    @patch('movies.get_directors')
-    def test_get_directors(self, mock_get_directors):
-        mock_get_directors.return_value = [{'id': 9339, 'name': 'Lilly Wachowski'}, {'id': 9340, 'name': 'Lana Wachowski'}]
-        result = movies.get_directors()
+    @patch('movies.tmdb.Movies')
+    def test_get_directors(self, mock_tmdb_Movies):
+        movie = MagicMock()
+        movie.credits.return_value = {'crew': [{
+            'gender': 1,
+            'id': 9339,
+            'known_for_department': 'Writing',
+            'name': 'Lilly Wachowski',
+            'department': 'Directing',
+            'job': 'Director'},
+            {
+            'gender': 1,
+            'id': 9340,
+            'known_for_department': 'Writing',
+            'name': 'Lana Wachowski',
+            'department': 'Directing',
+            'job': 'Director'}]}
+        result = movies.get_directors(movie)
         
         self.assertIsInstance(result, list, 'result should return a list')
         for item in result:
@@ -44,10 +69,15 @@ class TestMovies(unittest.TestCase):
                 self.assertIsInstance(k, str, 'dictionary key should be a str')
                 self.assertIsInstance(v, (str, int), 'key values can be str or int')
 
-    @patch('movies.get_cast')
-    def test_get_cast(self, mock_get_cast):
-        mock_get_cast.return_value = [{'id': 6384, 'name': 'Keanu Reeves'}, {'id': 2975, 'name': 'Laurence Fishburne'}, {'id': 530, 'name': 'Carrie-Anne Moss'}]
-        result = movies.get_cast()
+    @patch('movies.tmdb.Movies')
+    def test_get_cast(self, mock_tmdb_Movies):
+        movie = MagicMock()
+        movie.credits.return_value = {'cast': [
+            {'id': 6384, 'name': 'Keanu Reeves'}, 
+            {'id': 2975, 'name': 'Laurence Fishburne'}, 
+            {'id': 530, 'name': 'Carrie-Anne Moss'}
+            ]}
+        result = movies.get_cast(movie)
 
         self.assertIsInstance(result, list, 'result should return a list')
         for item in result:
@@ -56,13 +86,18 @@ class TestMovies(unittest.TestCase):
                 self.assertIsInstance(k, str, 'dictionary key should be a str')
                 self.assertIsInstance(v, (str, int), 'key values can be str or int')
 
-    @patch('movies.get_funders')
-    def test_get_funders(self, mock_get_funders):
-        mock_get_funders.return_value = (
-            [{'iso_3166_1': 'US', 'name': 'United States of America'}], 
-            [{'id': 420, 'name': 'Marvel Studios', 'origin_country': 'US'}]
-        )
-        result = movies.get_funders()
+    @patch('movies.tmdb.Movies')
+    def test_get_funders(self, mock_tmdb_Movies):
+        movie = MagicMock()
+        movie.info.return_value = {
+            'production_countries': [{'iso_3166_1': 'US', 'name': 'United States of America'}], 
+            'production_companies':[{'id': 79,
+            'logo_path': '/tpFpsqbleCzEE2p5EgvUq6ozfCA.png',
+            'name': 'Village Roadshow Pictures',
+            'origin_country': 'US'}]
+            }
+        
+        result = movies.get_funders(movie)
 
         for item in result:
             self.assertIsInstance(item, list, 'each result should be a list')
